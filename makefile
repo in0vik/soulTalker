d707e11716531aa54c2215e33270ce0b87d4eaaa
Ilya Novik <ilya.novikk@gmail.com>
2023-05-23 17:18:15 +0200

    refactor: remove after adding to gitignore

--- makefile
+++
@@ -1,5 +0,0 @@
-build:
-	docker build -t talktochatgpt .
-
-run:
-	docker run -d -p 3000:3000 --name talktochatgpt --rm talktochatgpt
--- src/utils.js
+++
@@ -1,9 +0,0 @@
-import { unlink } from 'fs/promises';
-
-export async function removeFile(filePath) {
-  try {
-    await unlink(filePath);
-  } catch (err) {
-    console.log('Error while removing file: ', err);
-  }
-}
